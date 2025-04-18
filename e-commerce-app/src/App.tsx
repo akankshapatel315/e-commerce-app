e728909c5ff032e839b3395940c8d93b0c46f313
akanksha701 <akanksha@upforce.tech>
2025-04-18 21:42:21 +0530

    initial layout setup

--- e-commerce-app/src/App.tsx
+++ e-commerce-app/src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
+import LoginPage from './components/login/LoginPage'
 
 function App() {
 
   return (
     <>
-     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md">
-        <h1 className="text-2xl font-bold text-blue-600">Hello Tailwind!</h1>
-        <p className="mt-2 text-gray-700">This is styled with Tailwind CSS</p>
-      </div>
-    </div>
+    <LoginPage/>
     </>
   )
 }
